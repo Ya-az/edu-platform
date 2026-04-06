17bd735fd16316e305ec8281062587d8e65c225f
Ya-az <Ya-az@users.noreply.github.com>
2026-04-06 09:35:56 +0300

    fix: make GitHub Pages deployment load correctly

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/edu-platform/',
+  base: './',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
